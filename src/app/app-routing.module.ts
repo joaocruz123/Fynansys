9feded984f690e383c9b760a475986b08a24bf8a
joaocruz123 <joaofranco.cruz@gmail.com>
2020-04-07 14:23:06 -0300

    Ajuste do navbar da aplicação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [
-  { path: 'categories', loadChildren:'./pages/categories/categories.module#CategoriesModule'}
+  { path: 'categories', loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule) }
 ];
 
 @NgModule({
